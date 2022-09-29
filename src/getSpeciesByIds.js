7dffc1c9ba6b4a115a866dac247e55ca174f3c5a
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-09-29 16:18:24 -0300

    requisito 1 pela metade

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ((ids.length === 0) ? [] : species.filter(((element) => ids.some(animal) => animal.id === id)));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
 
 module.exports = getSpeciesByIds;
