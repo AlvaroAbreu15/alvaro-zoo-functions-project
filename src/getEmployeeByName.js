7226fb47979e516530390654936429f415bb55e6
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-09-30 18:44:49 -0300

    requisito 3 terminado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return (employeeName === undefined) ? {} : data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,10 +2,7 @@ const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const result = [];
-  species.forEach((element, index) => {
-    (element.id === ids) ? result.push(element): element;
-  });
+  return species.filter((element) => element.id === ids);
 }
 // getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce')
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
