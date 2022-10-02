9ed24a56074c1a960456493a2333f8ce2ff713b0
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-02 00:03:29 -0300

    requisito 1 terminado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,9 +2,9 @@ const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((element) => element.id === ids);
+  const arr = [];
+  return (ids !== undefined) ? species.filter((element, index) => element.id === ids[0] || element.id === ids[1]) : arr;
 }
-// getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce')
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;
