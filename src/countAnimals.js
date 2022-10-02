1047d795527fa171c60f198f1cd429517e0ef526
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-02 14:47:38 -0300

    lint consertado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,27 +1,23 @@
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
-const { residents } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-   
-   if (animal !== undefined) {
-      if (Object.keys(animal).length === 1) {
-         return species
+  if (animal !== undefined) {
+    if (Object.keys(animal).length === 1) {
+      return species
         .filter((element) => element.name === animal.specie)[0]
-         .residents.length;
-      }
-      return species.filter((element) => element.name === animal.specie)[0].residents
-        .filter((j) => j.sex === animal.sex).length;
-   } 
-   let obj = {};
-   species.forEach((e) =>  {
-     obj[e.name] = e.residents.length
-   });
-   return obj;
-} 
-  
+        .residents.length;
+    }
+    return species.filter((element) => element.name === animal.specie)[0].residents
+      .filter((j) => j.sex === animal.sex).length;
+  }
+  const obj = {};
+  species.forEach((e) => {
+    obj[e.name] = e.residents.length;
+  });
+  return obj;
+}
+
 console.log(countAnimals());
-
-
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -6,16 +6,14 @@ function isManager(id) {
   return found;
 }
 
-function getRelatedEmployees(managerId, isManager) {
+function getRelatedEmployees(managerId) {
   if (isManager === true) {
-  const id = employees.filter((people) => people.managers.includes(managerId));
+    const id = employees.filter((people) => people.managers.includes(managerId));
 
-  return id.map((j) => `${j.firstName} ${j.lastName}`);
-  } 
+    return id.map((j) => `${j.firstName} ${j.lastName}`);
+  }
 
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
-console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
