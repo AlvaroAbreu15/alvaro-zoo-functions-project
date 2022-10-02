65b6fa9e5a8c5ab9e2b1bab1a0749ebe0dbb2e93
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-01 23:40:29 -0300

    finalizando o requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const found = employees.some((object) => object.managers.includes(id));
+  return found;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const id = employees.filter((people) => people.managers.includes(managerId));
+
+  return id.map((j) => `${j.firstName} ${j.lastName}`);
 }
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+
 module.exports = { isManager, getRelatedEmployees };
+ 
