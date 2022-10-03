93931bbeea89ab66c431dd92b03f560e9fceff4b
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-03 13:29:57 -0300

    requisito 8 terminado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
+// const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const zero = 0;
+  if ((entrants === undefined) || (Object.keys(entrants).length === 0)) {
+    return zero;
+  }
+  const childs = entrants.filter((element) => element.age < 18).length;
+  const adults = entrants.filter((e) => e.age >= 18 && e.age < 50).length;
+  const seniors = entrants.filter((j) => j.age >= 50).length;
+  const obj = { child: childs, adult: adults, senior: seniors };
+  return obj;
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if ((entrants === undefined) || (Object.keys(entrants).length === 0)) {
+    return 0;
+  }
+  const childrens = entrants.filter((element) => element.age < 18);
+  const adults = entrants.filter((e) => e.age >= 18 && e.age < 50).length;
+  const seniors = entrants.filter((j) => j.age >= 50).length;
+  return (childrens.length * 20.99) + (adults * 49.99) + (seniors * 24.99);
 }
 
 module.exports = { calculateEntry, countEntrants };
