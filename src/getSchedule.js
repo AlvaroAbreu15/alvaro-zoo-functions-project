e5153baeaff34fa2a87af9d2e171658916850e52
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-03 18:38:58 -0300

    comecando requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+
+  // if(Object.keys(obj).includes(scheduleTarget)) {
+  //  return obj.filter((element) => element[scheduleTarget])
+  // }
+
+  return species.find((e) => e.name === scheduleTarget).availability;  /// OK
+
+  
+  
 }
+console.log(getSchedule('lions'));
+
 
 module.exports = getSchedule;
