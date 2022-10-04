3a321c156085e32dd726e176bc467e4c34f0eb5c
AlvaroAbreu15 <alvaroabreu1509@gmail.com>
2022-10-04 17:21:47 -0300

    requisito 10 terminado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,10 +3,17 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const animalId = employees.filter((e) => e.id === id)[0].responsibleFor[0];
-
   const animal = species.find((j) => j.id === animalId).residents;
-  animal.reduce((acc, curr) => 
-  , [])
+
+  const fera = animal.reduce((acc, curr) => {
+   if (curr.age > acc){
+    return curr.age;
+   } 
+    return acc;
+   }, []);
+
+   const finalObject = animal.filter((el) => el.age === fera)[0];
+   return [finalObject.name, finalObject.sex, finalObject.age];
 }
 
 console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
